b0995f432cdde48731276d44cfcb482b82c1cef9
Walid Amro <60905923+walidamro-msft@users.noreply.github.com>
2025-04-08 21:40:22 +0000

    Implement hotel and booking retrieval endpoints in the API

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER "id-psouae323gdls" FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER "id-psouae323gdls";
+ALTER ROLE db_datawriter ADD MEMBER "id-psouae323gdls";
 
